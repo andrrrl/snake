b7f4889a8cebf29c993578fbc6b0db17160275c1
Andrés Velázquez Boc-Ho <avelazquez@hanwhawisenet.com>
2023-08-24 16:39:23 -0300

    feat: advanced game settings

--- snake.js
+++ snake.js
@@ -1,21 +1,40 @@
-const boardDiv = document.querySelector('.board')
-
+/**
+ * This code snippet sets up a game board and controls the movement of a snake on the board.
+ * It defines a `boardConfig` object with properties for board dimensions, cell size, movement rates, snake food, and game state.
+ * It also defines a `snake` object with properties and methods for controlling the snake's movement.
+ * The code sets up the game board by styling and positioning the board div element.
+ * It adds an event listener for keydown events to control the snake's movement direction.
+ * The snake's initial position, length, and tail are set, and the game loop is started.
+ * The game loop repeatedly calls the `move` method of the `snake` object to update the snake's position and check for collisions.
+ * Snake food is displayed on the board and can be eaten by the snake, causing the snake's tail to grow.
+ * The game loop continues until the snake collides with a wall or its own tail and is marked as dead.
+ */
 const boardConfig = {
     dimensions: { x: 500, y: 500 },
     cellSize: 10,
     movementRatePixels: 10,
     movementRateMs: 100,
     startAxisDirection: 'x',
-    snakeFood: [
-        { x: 200, y: 80, value: 1, eaten: false, current: true },
-        { x: 200, y: 180, value: 2, eaten: false, current: false },
-        { x: 300, y: 10, value: 3, eaten: false, current: false },
-        { x: 30, y: 300, value: 3, eaten: false, current: false },
-        { x: 100, y: 450, value: 3, eaten: false, current: false },
-        { x: 400, y: 300, value: 3, eaten: false, current: false },
-    ],
+    getRandom: (axis) => {
+        const baseNumber = Math.floor(Math.random(0, boardConfig.dimensions[axis]) * boardConfig.dimensions[axis]).toString().split('')
+        baseNumber.pop()
+        baseNumber.push('0')
+        return Number(baseNumber.join(''))
+    },
+    foodQuantity: 10,
+    snakeFood: [],
+    boardDiv: document.querySelector('.board'),
     foodDiv: document.createElement('div'),
     setSnakeFood: () => {
+        if (boardConfig.snakeFood.length === 0) {
+            boardConfig.snakeFood = Array.from(Array(boardConfig.foodQuantity)).map(x => ({
+                x: boardConfig ? boardConfig.getRandom('x') : 0,
+                y: boardConfig ? boardConfig?.getRandom('y') : 0,
+                value: 1,
+                eaten: false,
+                current: true
+            }))
+        }
         const food = boardConfig.snakeFood.find(x => x.eaten === false)
         food.current = true
         boardConfig.foodDiv.style.width = `${boardConfig.cellSize}px`
@@ -25,12 +44,12 @@ const boardConfig = {
         boardConfig.foodDiv.style.position = 'absolute'
         boardConfig.foodDiv.style.left = `${food.x}px`
         boardConfig.foodDiv.style.top = `${food.y}px`
-        boardDiv.appendChild(boardConfig.foodDiv)
+        boardConfig.boardDiv.appendChild(boardConfig.foodDiv)
     },
     removeSnakeFood: (food) => {
         const index = boardConfig.snakeFood.indexOf(food)
         boardConfig.snakeFood = boardConfig.snakeFood.slice(index)
-        boardDiv.removeChild(boardConfig.foodDiv)
+        boardConfig.boardDiv.removeChild(boardConfig.foodDiv)
         if (boardConfig.snakeFood.filter(x => x.eaten === false).length > 0) {
             boardConfig.setSnakeFood()
         } else {
@@ -44,8 +63,8 @@ const boardConfig = {
     gameOver: false
 }
 
-boardDiv.style.width = `${boardConfig.dimensions.x}px`
-boardDiv.style.height = `${boardConfig.dimensions.y}px`
+boardConfig.boardDiv.style.width = `${boardConfig.dimensions.x}px`
+boardConfig.boardDiv.style.height = `${boardConfig.dimensions.y}px`
 
 document.onkeydown = (e) => {
     if (snake.isDead) {
@@ -88,8 +107,10 @@ const snake = {
     tail: [],
     pathCoords: [],
     move: () => {
-        if (snake.wallCollision() || snake.isDead) {
+        if (!snake.isDead && snake.wallCollision() || snake.tailBite()) {
             snake.viewContainer.style.background = 'red'
+            snake.isDead = true
+            boardConfig.gameOver = true
             return
         }
 
@@ -112,7 +133,7 @@ const snake = {
             snake.pathCoords.shift()
         }
 
-        snake.moveTail(boardConfig.eatenFood, { left: snake.currentPosition.left, top: snake.currentPosition.top, ...snake.currentDirection })
+        snake.moveTail({ left: snake.currentPosition.left, top: snake.currentPosition.top, ...snake.currentDirection })
 
     },
     setMoveTo: (newDirection) => {
@@ -129,6 +150,7 @@ const snake = {
             edibleFood.eaten = true
             edibleFood.current = false
             boardConfig.removeSnakeFood(edibleFood)
+            boardConfig.movementRateMs -= 7.5
             return true
         }
         return false
@@ -139,9 +161,16 @@ const snake = {
         const collisionNeg = snake.currentPosition[snake.currentDirection.cssProp] <= -boardConfig.cellSize
 
         if (collisionPos || collisionNeg) {
-            snake.isDead = true
+            alert('GAME OVER: ⚡️ you hit an electrified wall!')
         }
         return collisionPos || collisionNeg
+    },
+    tailBite: () => {
+        const bite = snake.pathCoords.length !== new Set(snake.pathCoords.map(x => `top-${x.top}-left-${x.left}`)).size
+        if (bite) {
+            alert('GAME OVER: 💀 you bit yourself!')
+        }
+        return bite
     },
     isDead: false,
     viewContainer: document.querySelector('.snake'),
@@ -163,7 +192,7 @@ const snake = {
             snake.tail.push({ id, coords })
         }
     },
-    moveTail: (id, newCoord) => {
+    moveTail: (newCoord) => {
         snake.tail = snake.tail.map(x => {
             return { ...x, coords: newCoord }
         })
@@ -196,14 +225,14 @@ boardConfig.setSnakeFood()
 snake.growTail(boardConfig.eatenFood, { left: snake.currentPosition.left, top: snake.currentPosition.top })
 
 const game = (function loop() {
-    if (!boardConfig.gameOver) {
-        setTimeout(() => {
+    setTimeout(() => {
+        if (!boardConfig.gameOver) {
             coordsView.querySelector('.x span').innerHTML = snake.currentPosition.left
             coordsView.querySelector('.y span').innerHTML = snake.currentPosition.top
             snake.move()
             loop()
-        }, boardConfig.movementRateMs)
-    }
+        }
+    }, boardConfig.movementRateMs)
 })
 
 game()
